6f3c77e94e83885e6ada5b941fafdcd3b5cc0aed
MikeHRM <michael.filippo@pl.abb.com>
2025-10-21 16:35:04 +0200

    init express project

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+// const router = app.router()
+const port = 3001;
+
+app.get("/", (req, res) => {
+  // logic
+
+  // res.status(500).json({error: 'an error has occured' })
+  res.status(200).json({
+    message: "hello world!",
+  });
+});
+
+app.post("/", (req, res) => {
+  // res.status(500).json({error: 'an error has occured' })
+  res.status(200).json({
+    number: Math.random(),
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
