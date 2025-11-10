078134044e1f8c29cd6689fb4ab2313cc46a9ce9
MikeHRM <michael.filippo@pl.abb.com>
2025-11-10 16:43:51 +0100

    play with bcrypt

--- users/index.js
+++ users/index.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import { v4 } from "uuid";
+import bcrypt from "bcrypt";
 
 // recreate __filename and __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
@@ -14,7 +16,7 @@ const apiRouter = Router();
  * @param {string} email  has to be a valid email
  * @param {string} password has to fullfill the password strength
  */
-apiRouter.post("/register", (req, res) => {
+apiRouter.post("/register", async (req, res) => {
   // bad request - cannot continue
   if (!req.body.email || !req.body.password) {
     res.status(400).json({
@@ -24,6 +26,7 @@ apiRouter.post("/register", (req, res) => {
     // verify if the user with given email already exists
 
     // crypt password
+    const cryptedPassword = await bcrypt.hash(req.body.password, 5);
 
     // add user to DB
     const filePath = path.join(__dirname, "../db/db.json");
@@ -32,18 +35,28 @@ apiRouter.post("/register", (req, res) => {
     const newUsers = [
       ...data.users,
       {
+        id: v4(),
         email: req.body.email,
-        password: req.body.password,
+        password: cryptedPassword,
       },
     ];
 
     fs.writeFileSync(filePath, JSON.stringify({ users: newUsers }));
 
     res.status(200).json({
-      message: "hello from register",
-      body: `The body is ${JSON.stringify(newUsers)}`,
+      message: "registration successfull",
     });
   }
+});
+
+apiRouter.get("/", async (req, res) => {
+  const message = await bcrypt.compare(
+    "pluto",
+    "$2b$05$GONdPNvXn5ffK5N5DXwMme0nH2tVWOHauD50SY0G6qpvVEe8yOVOq"
+  );
+  res.status(200).json({
+    message,
+  });
 });
 
 export default apiRouter;
