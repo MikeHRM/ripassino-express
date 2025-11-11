f7c380a4e715a340b24f8cf3f55910158ba623c0
MikeHRM <michael.filippo@pl.abb.com>
2025-11-11 16:46:47 +0100

    add jwt and login route

--- users/index.js
+++ users/index.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import fs from "node:fs";
+import fs, { read } from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { v4 } from "uuid";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // recreate __filename and __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
@@ -23,29 +24,95 @@ apiRouter.post("/register", async (req, res) => {
       message: "Please provide both email and password",
     });
   } else {
-    // verify if the user with given email already exists
-
-    // crypt password
-    const cryptedPassword = await bcrypt.hash(req.body.password, 5);
-
     // add user to DB
     const filePath = path.join(__dirname, "../db/db.json");
     const data = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 
-    const newUsers = [
-      ...data.users,
-      {
-        id: v4(),
-        email: req.body.email,
-        password: cryptedPassword,
-      },
-    ];
+    // verify if the user with given email already exists
+    const userAlreadyExists = data.users.find(
+      (user) => user.email === req.body.email
+    );
 
-    fs.writeFileSync(filePath, JSON.stringify({ users: newUsers }));
+    if (userAlreadyExists) {
+      res.status(407).json({
+        message: "user already exists",
+      });
+    } else {
+      // crypt password
+      const cryptedPassword = await bcrypt.hash(req.body.password, 5);
 
-    res.status(200).json({
-      message: "registration successfull",
+      const newUsers = [
+        ...data.users,
+        {
+          id: v4(),
+          email: req.body.email,
+          password: cryptedPassword,
+        },
+      ];
+
+      fs.writeFileSync(filePath, JSON.stringify({ users: newUsers }));
+
+      res.status(200).json({
+        message: "registration successfull",
+      });
+    }
+  }
+});
+
+/**
+ * @description POST method for authenticating an existing user
+ * @param {string} email  has to be a valid email
+ * @param {string} password has to fullfill the password strength
+ */
+apiRouter.post("/login", async (req, res) => {
+  // bad request - cannot continue
+  if (!req.body.email || !req.body.password) {
+    res.status(400).json({
+      message: "Please provide both email and password",
     });
+  } else {
+    // add user to DB
+    const filePath = path.join(__dirname, "../db/db.json");
+    const data = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+
+    // verify if the user with given email already exists
+    const userAlreadyExists = data.users.find(
+      (user) => user.email === req.body.email
+    );
+
+    if (!userAlreadyExists) {
+      console.log("user does not exist");
+      res.status(407).json({
+        message: "authentication failed",
+      });
+    } else {
+      // verify password
+      const isPasswordCorrect = await bcrypt.compare(
+        req.body.password,
+        userAlreadyExists.password
+      );
+
+      // if correct => token
+      if (isPasswordCorrect) {
+        const token = jwt.sign(
+          {
+            email: req.body.email,
+          },
+          "shhhhh"
+        );
+
+        res.status(200).json({
+          message: "ok",
+          token,
+        });
+      } else {
+        console.log("password does not match");
+        // if error =>
+        res.status(407).json({
+          message: "authentication failed",
+        });
+      }
+    }
   }
 });
 
